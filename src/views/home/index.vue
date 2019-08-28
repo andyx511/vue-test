30de08c26f284c3927f7a1c972f27a912340f10c
nidingbo <dingbo.ni@dtp1688.com>
2019-08-28 16:43:43 +0800

    第一次的提交

---
+++ src/views/home/index.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="dashboard-container">
+    <div class="block">
+      <span class="demonstration">默认</span>
+      <el-date-picker
+        v-model="value1"
+        type="datetime"
+        placeholder="选择日期时间">
+      </el-date-picker>
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  data() {
+    return {
+      pickerOptions: {
+        shortcuts: [{
+          text: '今天',
+          onClick(picker) {
+            picker.$emit('pick', new Date())
+          }
+        }
+        ]
+      },
+      value1: ''
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.dashboard {
+  &-container {
+    margin: 30px;
+  }
+  &-text {
+    font-size: 30px;
+    line-height: 46px;
+  }
+}
+</style>
